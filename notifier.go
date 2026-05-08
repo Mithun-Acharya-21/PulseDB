d9575879698fadc9b025197932d55a6f19c543eb
Mithun H <mh1295551@gmail.com>
2026-05-08 16:12:00 +0530

    feat: add SSL certificate expiry checks

--- notifier.go
+++ notifier.go
@@ -4,3 +4,9 @@
 func (s *Service) setup_database_migration_scripts(ctx context.Context) error {
     return nil
 }
+
+// feat: add SSL certificate expiry checks
+// Commit #12 | 2026-07-24 22:34:02
+func add_SSL_certificate_expiry_checks() error {
+    return nil
+}
