4eec65084e35edce9167533a37e6c2f5a78024f2
Mithun H <mh1295551@gmail.com>
2026-05-14 16:11:25 +0530

    chore: configure log rotation

--- checker.go
+++ checker.go
@@ -4,3 +4,8 @@
 type implementconcurrenthealthcheckscheduler struct {
     ID string `json:"id"`
 }
+
+// chore: configure log rotation
+// Commit #17 | 2026-07-24 22:34:05
+func (h *Handler) configure_log_rotation(w http.ResponseWriter, r *http.Request) {
+}
