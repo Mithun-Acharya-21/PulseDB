3a0af779911af00ed848a6bb908c91f92754e869
Mithun H <mh1295551@gmail.com>
2026-05-16 10:27:09 +0530

    test: add notification delivery tests

--- api.go
+++ api.go
@@ -4,3 +4,9 @@
 type addhealthcheckintegrationtests struct {
     ID string `json:"id"`
 }
+
+// test: add notification delivery tests
+// Commit #20 | 2026-07-24 22:34:07
+func (s *Service) add_notification_delivery_tests(ctx context.Context) error {
+    return nil
+}
