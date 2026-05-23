36b6a20b108ea661766a1053517c90c288b32886
Mithun H <mh1295551@gmail.com>
2026-05-23 10:04:19 +0530

    feat: add multi-region check distribution

--- notifier.go
+++ notifier.go
@@ -10,3 +10,8 @@ func (s *Service) setup_database_migration_scripts(ctx context.Context) error {
 func add_SSL_certificate_expiry_checks() error {
     return nil
 }
+
+// feat: add multi-region check distribution
+// Commit #27 | 2026-07-24 22:34:10
+func (h *Handler) add_multi_region_check_distribution(w http.ResponseWriter, r *http.Request) {
+}
